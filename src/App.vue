9d4164ba69d9cfcf5a3227bf689b8543535867d1
Chao Meng <cmeng14@myseneca.ca>
2023-09-02 14:19:34 -0400

    basic frame

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+   <!-- The button to acquire user's cuttent location-->
+   <button>Get Current Location</button>
+   <div>
+     <!-- The search module-->
+      <button>Go</button>
+   </div>
+  <!-- Display the location on the map-->
+  <div id="map" style="height: 600px; width: 100%;"></div>
+
+  <!-- A table with pagination to show searched places-->
+  <table>
+    <thead>
+      <tr>
+        <th>
+          <input type="checkbox" />
+        </th>
+        <th>Location</th>
+        <th>Time Zone</th>
+        <th>Local Time</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- Display 10 records per page-->
+      <tr>
+        <td>
+          <input type="checkbox" />
+        </td>
+        <td>name</td>
+        <td>timeZone</td>
+        <td>localtime</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <!-- Pagination buttons -->
+  <div>
+    <button></button>
+  </div>
+
+  <!-- Delete button to remove selected records and markers -->
+  <button>Detlete</button>
+
+  <!-- Display the time zone and local time of the latest searched location -->
+  <div>
+    <p></p>
+  </div>
+ </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
 
