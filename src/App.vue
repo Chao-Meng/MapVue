cee2e3ecf0abb086cdaa650a44acfbe833c68660
Chao Meng <cmeng14@myseneca.ca>
2023-09-08 15:46:53 -0400

    add some alert and edit the format

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,10 @@
      <input v-model = "searchLocation" @keyup.enter="handleSearch" placeholder="Enter a location" />
       <button id="map-button" @click="handleSearch">Go</button>
    </div>
+
 <!-- Delete button to remove selected records and markers -->
-  <button id="delete-button" @click="handleDelete">Delete</button>
+  <button id="delete-button" @click="showDeleteConfirmation">Delete</button>
+
   <!-- A table with pagination to show searched places-->
   <table id="custom-table">
     <thead>
@@ -46,17 +48,12 @@
   </table>
 
   <!-- Pagination buttons -->
-  <div>
-   <!-- <button id="page-button" v-for="page in totalPages" :key="page" @click="paginate(page)">{{page}}</button> -->
-   <div class="pagination">
-  <button v-for="page in filteredPages" :key="page" @click="paginate(page)">{{ page }}</button>
-</div>
 
+  <div class="pagination">
+    <button v-for="page in filteredPages" :key="page" @click="paginate(page)">{{ page }}</button>
   </div>
-
   
-
-  <!-- Display the time zone and local time of the latest searched location -->
+    <!-- Display the time zone and local time of the latest searched location -->
   <div v-if="latestSearchedLocation">
     <p>Searched location:</p>
     <p>Location: {{ latestSearchedLocation.name }}</p>
@@ -64,7 +61,7 @@
     <p>Local Time: {{ latestSearchedLocation.localTime }}</p>
   </div>
  </div>
-  
+ 
 </template>
 
 <script>
@@ -73,7 +70,7 @@ export default {
  data() {
    return {
      userLocation: null,
-      searchLocation: '',// 用于存储用户输入的位置名称
+      searchLocation: '',// store the location name entered by the user
       searchedLocations: [],
       selectAll: false,
       selectedLocations: [],
@@ -90,49 +87,41 @@ export default {
    };
  },
  methods: {
-   getUserLocation() {
-     
+   getUserLocation() {  
      if("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition((position) =>{
-        // this.$geolocation
-        // .getCurrentPosition()
-        // .then((position) => {
          const longitude = position.coords.longitude;
          const latitude = position.coords.latitude;
-
-         //this.userLocation = {longitude,latitude};
          const userLocation = new window.google.maps.LatLng(latitude, longitude);
-           this.map.setZoom(16);
+        this.map.setZoom(16);
         //Remove the previous marker.
          if (this.marker) {
-          this.marker.setMap(null);
+          //this.marker.setMap(null);
+          this.marker.setVisible(false)
           this.marker = null;
-        }
-         
+        }        
          this.marker =  new window.google.maps.Marker({
           position:userLocation,
           map: this.map,
           title: 'Marker Title',
-        });
-       
+        });      
         this.map.setCenter(userLocation);
        },
        (error) => {
          console.error("Error getting user location:", error.message);
-      }
-       );
+        });
      }else {
        alert ("This browser does not support Geolocation, please change a browser");
      }
    },
+
    initMap() {
     const apiKey = 'AIzaSyBnOZude3pec_o9QgQhDNM7d2HNZ8LlGbI';
     const script = document.createElement('script');
     script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
     script.defer = true;
     script.async = true;
-    script.onload = () => {
-      
+    script.onload = () => {    
     const mapOptions = {
           center: { lat: 43.74822, lng: -79.28941 },
           zoom: 14,
@@ -143,76 +132,69 @@ export default {
       document.head.appendChild(script);
     },
  
-handleSearch() {
-  const locationName = this.searchLocation.trim();
-
-  if (locationName) {
-    const formattedLocationName = locationName.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
-    const existingLocationIndex = this.searchedLocations.findIndex(
-      (location) => location.name === formattedLocationName
-    );
-
-    // Creating geocoding services
-    const geocoder = new window.google.maps.Geocoder();
-    // Converting addresses to latitude and longitude using geocoding services
-    geocoder.geocode({ address: locationName }, (results, status) => {
-      if (status === 'OK' && results[0]) {
-        // Get the geographic coordinates of the first result
-        const location = results[0].geometry.location;
-        const latitude = location.lat();
-        const longitude = location.lng();
-
- /*Display the location on a map and add a marker to 
-  //           each searched location every time the location changes.*/
-            this.marker = new window.google.maps.Marker({
-              position: location,
-              map: this.map,
-              title: locationName,
-            });
-
-            this.markers.push(this.marker);
-            //Move the window view to the location of the search results
-            this.map.setCenter(location);
-
-
-
-        // Check if the location already exists
-        if (existingLocationIndex !== -1) {
-          const existingLocation = this.searchedLocations[existingLocationIndex];
-          this.updateLocationDetails(existingLocation, latitude, longitude);
+  handleSearch() {
+    const locationName = this.searchLocation.trim();
+    if (locationName) {
+      const formattedLocationName = locationName.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+      const existingLocationIndex = this.searchedLocations.findIndex(
+        (location) => location.name === formattedLocationName
+      );
+      // Creating geocoding services
+      const geocoder = new window.google.maps.Geocoder();
+      // Converting addresses to latitude and longitude using geocoding services
+      geocoder.geocode({ address: locationName }, (results, status) => {
+        if (status === 'OK' && results[0]) {
+          // Get the geographic coordinates of the first result
+          const location = results[0].geometry.location;
+          const latitude = location.lat();
+          const longitude = location.lng();
+          /*Display the location on a map and add a marker to 
+          // each searched location every time the location changes.*/
+          this.marker = new window.google.maps.Marker({
+            position: location,
+            map: this.map,
+            title: locationName,
+          });
+          this.markers.push(this.marker);
+          //Move the window view to the location of the search results
+          this.map.setCenter(location);
+          // Check if the location already exists
+          if (existingLocationIndex !== -1) {
+            const existingLocation = this.searchedLocations[existingLocationIndex];
+            this.updateLocationDetails(existingLocation, latitude, longitude);
+          } else {
+            this.addNewLocation(formattedLocationName, latitude, longitude);
+          }
         } else {
-          this.addNewLocation(formattedLocationName, latitude, longitude);
+          console.error('Geocode was not successful for the following reason: ' + status);
+          alert('City not found. Please enter a valid city name.');
         }
-      } else {
-        console.error('Geocode was not successful for the following reason: ' + status);
-        alert('City not found. Please enter a valid city name.');
-      }
-    });
-  } else {
-    console.log('Please enter a location name.');
-  }
-},
-updateLocationDetails(existingLocation, latitude, longitude) {
-    fetch(`https://maps.googleapis.com/maps/api/timezone/json?location=${latitude},${longitude}&timestamp=${Date.now() / 1000}&key=AIzaSyBnOZude3pec_o9QgQhDNM7d2HNZ8LlGbI&timeZoneName=true`)
-      .then(response => response.json())
-      .then(data => {
-        const cityTimeZone = data.timeZoneId;
-        const localTime = DateTime.now().setZone(cityTimeZone).toFormat('yyyy-MM-dd HH:mm:ss');
-      //  const cityTimeZone = data.timeZoneId;
-      //   const zone = IANAZone.create(cityTimeZone);
-      //   const localTime = DateTime.now().setZone(zone).toFormat('yyyy-MM-dd HH:mm:ss');
-       existingLocation.timeZone = cityTimeZone;
-        existingLocation.localTime = localTime;
-        this.moveLocationToTop(existingLocation);
-        this.searchLocation = ''
-      })
-      .catch(error => {
-        console.error("Error fetching time zone data:", error);
       });
+    } else {
+      console.log('Please enter a location name.');
+    }
   },
-  
+//If the city the user searched for already exists, update the information and move its position to the top of the table.
+  updateLocationDetails(existingLocation, latitude, longitude) {
+      fetch(`https://maps.googleapis.com/maps/api/timezone/json?location=${latitude},${longitude}&timestamp=${Date.now() / 1000}
+      &key=AIzaSyBnOZude3pec_o9QgQhDNM7d2HNZ8LlGbI&timeZoneName=true`)
+        .then(response => response.json())
+        .then(data => {
+          const cityTimeZone = data.timeZoneId;
+          const localTime = DateTime.now().setZone(cityTimeZone).toFormat('yyyy-MM-dd HH:mm:ss');
+          existingLocation.timeZone = cityTimeZone;
+          existingLocation.localTime = localTime;
+          this.moveLocationToTop(existingLocation);
+          this.searchLocation = ''
+        })
+        .catch(error => {
+          console.error("Error fetching time zone data:", error);
+        });
+    },
+  //If the city the user searched for does not exist, add new information about it.
   addNewLocation(name, latitude, longitude) {
-    fetch(`https://maps.googleapis.com/maps/api/timezone/json?location=${latitude},${longitude}&timestamp=${Date.now() / 1000}&key=AIzaSyBnOZude3pec_o9QgQhDNM7d2HNZ8LlGbI&timeZoneName=true`)
+    fetch(`https://maps.googleapis.com/maps/api/timezone/json?location=${latitude},${longitude}&timestamp=${Date.now() / 1000}
+    &key=AIzaSyBnOZude3pec_o9QgQhDNM7d2HNZ8LlGbI&timeZoneName=true`)
       .then(response => response.json())
       .then(data => {
         const cityTimeZone = data.timeZoneId;
@@ -222,8 +204,6 @@ updateLocationDetails(existingLocation, latitude, longitude) {
           name: name,
           timeZone: cityTimeZone,
           localTime: localTime,
-          // lat: latitude,
-          // lng: longitude,
         };
         this.searchedLocations.unshift(searchResult);
         this.displayedLocations = this.searchedLocations.slice(0, 9);
@@ -233,8 +213,9 @@ updateLocationDetails(existingLocation, latitude, longitude) {
       .catch(error => {
         console.error("Error fetching time zone data:", error);
       });
-  },
-  
+    },
+
+  //Update the most recently research to the top of the table.
   moveLocationToTop(location) {
     const index = this.searchedLocations.indexOf(location);
     if (index !== -1) {
@@ -244,31 +225,26 @@ updateLocationDetails(existingLocation, latitude, longitude) {
     }
   },
 
-    handleSelectAll() {
-      
-      if (this.selectAll) {
-        // 如果全选被勾选，将当前页的所有行都加入 selectedLocations
+  handleSelectAll() {  
+    if (this.selectAll) {
+    // If Select All is checked, adds all rows on the current page to the selectedLocations
       this.selectedLocations = [...this.displayedLocations];
-      
-      }else{
-        // 如果全选被取消，清空 selectedLocations
-         this.selectedLocations = [];
-          
-      }
-     this.updateSelectAll();
-    },
+    }else{
+      // Clear selectedLocations if unselected any.
+      this.selectedLocations = [];   
+    }
+    this.updateSelectAll();
+  },
 
-    updateSelectAll() {
-    const allCurrentPageRowsSelected = this.displayedLocations.every((location) =>
-        this.selectedLocations.includes(location)
-      );
+  updateSelectAll() {
+  const allCurrentPageRowsSelected = this.displayedLocations.every((location) =>
+      this.selectedLocations.includes(location)
+    );
+    // Auto-check/uncheck all boxes
+    this.selectAll = allCurrentPageRowsSelected;
+  },
 
-      // 自动勾选/取消全选框
-      this.selectAll = allCurrentPageRowsSelected;
-},
-
-    handleSelect(location) {
-      
+  handleSelect(location) {  
     const index = this.selectedLocations.indexOf(location);
       if (index !== -1) {
         this.selectedLocations.splice(index, 1);
@@ -276,107 +252,119 @@ updateLocationDetails(existingLocation, latitude, longitude) {
         this.selectedLocations.push(location);
       }
       this.updateSelectAll();
+  },
 
-    },
+//Display a alert box prompting the user whether delete or not.
+ showDeleteConfirmation() {
+    if (this.selectedLocations.length > 0) {
+      const confirmMessage = "Are you sure you want to delete?";
+      const userConfirmed = confirm(confirmMessage);
+      if (userConfirmed) {
+        //if user click OK button, perform the delete operation
+        this.executeDelete();
+      } 
+    } else {
+      //If the user does not check any valid rows and clicks the delete button, an alert is given.  
+      alert(' Please check the information you want to delete first');
+    }
+  },
 
-    handleDelete() {
-  if (this.selectedLocations.length > 0) {
-    const locationsToDelete = [...this.selectedLocations]; // 创建要删除的项的副本
-
-    locationsToDelete.forEach((locationToDelete) => {
-      const nameToRemove = locationToDelete.name ? locationToDelete.name.toLowerCase() : null; // 将地点名称转换为小写，如果为空则为null
- 
+  executeDelete() {
+    const locationsToDelete = [...this.selectedLocations];
+   locationsToDelete.forEach((locationToDelete) => {
+      // Convert location names to lowercase or null if empty
+      const nameToRemove = locationToDelete.name ? locationToDelete.name.toLowerCase() : null; 
       if (nameToRemove !== null) {
-        // 使用 forEach 循环删除 markers 数组中的标记
+        // Use a forEach loop to remove markers from the markers array.
         this.markers.forEach((marker, index) => {
-          const markerTitle = marker.getTitle() ? marker.getTitle().toLowerCase() : null; // 将标记的 title 属性转换为小写，如果为空则为null
+          // Convert the tag's title attribute to lowercase, or null if it's empty
+          const markerTitle = marker.getTitle() ? marker.getTitle().toLowerCase() : null; 
           console.log('move',markerTitle,nameToRemove);
           if (markerTitle === nameToRemove) {
-           // marker.setMap(null); // 从地图上移除标记
+           // marker.setMap(null); // Remove marker from map
            marker.setVisible(false);
-
-            console.log('move',marker);
-            this.markers.splice(index, 1); // 从 markers 数组中移除标记
+           console.log('move',marker);
+          // Remove marker from markers
+            this.markers.splice(index, 1); 
           }
         });
 
-        // 从 searchedLocations 中移除选中行
+        // remove the selected row from searchedLocations 
         const indexToRemove = this.searchedLocations.findIndex((loc) => (loc.name ? loc.name.toLowerCase() : null) === nameToRemove);
         if (indexToRemove !== -1) {
           this.searchedLocations.splice(indexToRemove, 1);
         }
 
-        // 从 displayedLocations 中移除选中行
+        //remove the selected row from displayedLocations
         const displayedIndexToRemove = this.displayedLocations.findIndex((loc) => (loc.name ? loc.name.toLowerCase() : null) === nameToRemove);
         if (displayedIndexToRemove !== -1) {
           this.displayedLocations.splice(displayedIndexToRemove, 1);
         }
+        this.rowsSelected = false;
       }
+      
     });
 
-    // 清空选中的地点
+    // Clear the selected locations
     this.selectedLocations = [];
     this.selectAll = false;
-    // 更新 displayedLocations 以包含剩余的记录
+    this.rowsSelected = false;
+
+    //update displayedLocations to show the remaining records
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
     this.displayedLocations = this.searchedLocations.slice(startIndex, endIndex);
-  }
-},
-   
+  },
     paginate(page) {
-  const startIndex = (page - 1) * this.itemsPerPage;
-  const endIndex = startIndex + this.itemsPerPage;
-  const totalPages = Math.ceil(this.searchedLocations.length / this.itemsPerPage);
-console.log('totalpages',totalPages);
-  if (page > totalPages) {
-    // 如果删除后的总页数小于当前页码，则将当前页码设置为最后一页
-    this.currentPage = totalPages;
-    console.log('currentpage',this.currentPage);
-  } else {
-    this.currentPage = page;
-    console.log('currentpage',this.currentPage);
-  }
-
-  this.displayedLocations = this.searchedLocations.slice(startIndex, endIndex);
-},
-
-toggleMarker(location) {
-      const marker = this.findMarkerForLocation(location);
-
-      if (marker) {
-        // 获取被点击标记的位置坐标
-        const markerPosition = marker.getPosition();
-        
-        // 使用地图API缩放地图并将中心设置为标记位置
-        this.map.setZoom(14); // 适当的缩放级别
-        this.map.setCenter(markerPosition);
+      const startIndex = (page - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      const totalPages = Math.ceil(this.searchedLocations.length / this.itemsPerPage);
+      console.log('totalpages',totalPages);
+      if (page > totalPages) {
+        /* If the total number of pages after deletion is less than the current page number
+        set the current page number as the last page*/
+        this.currentPage = totalPages;
+        console.log('currentpage',this.currentPage);
+      } else {
+        this.currentPage = page;
+        console.log('currentpage',this.currentPage);
       }
-    },
+      this.displayedLocations = this.searchedLocations.slice(startIndex, endIndex);
+  },
 
-    isSelected(location) {
-      return this.selectedLocations.includes(location);
-    },
+  toggleMarker(location) {
+    const marker = this.findMarkerForLocation(location);
+    if (marker) {
+      // Get the coordinates of the clicked marker's position
+      const markerPosition = marker.getPosition(); 
+      // Use the Maps API to zoom the map and set the center to the marker location
+      this.map.setZoom(14); 
+      this.map.setCenter(markerPosition);
+    }
+  },
 
-    findMarkerForLocation(location) {
-      // 根据location查找匹配的marker
-      return this.markers.find(
-        (marker) => marker.getTitle().toLowerCase() === location.name.toLowerCase()
-      );
-    },
+  isSelected(location) {
+    return this.selectedLocations.includes(location);
+  },
 
- },
- mounted() {
-   // Initialize the map
-  this.initMap();
- },
- //创建一个计算属性来返回过滤后的页码数组：
-computed: {
-  filteredPages() {
-    const totalPages = Math.ceil(this.searchedLocations.length / this.itemsPerPage);
-    return Array.from({ length: totalPages }, (_, index) => index + 1).filter(page => page > 0);
+  findMarkerForLocation(location) {
+    // Find matching markers based on location
+    return this.markers.find(
+      (marker) => marker.getTitle().toLowerCase() === location.name.toLowerCase()
+    );
+  },
+  },
+  mounted() {
+    // Initialize the map
+    this.initMap();
+  },
+ //Creates a calculated property to return an array of filtered page numbers.
+  computed: {
+    filteredPages() {
+      const totalPages = Math.ceil(this.searchedLocations.length / this.itemsPerPage);
+      return Array.from({ length: totalPages }, (_, index) => index + 1).filter(page => page > 0);
+    }
   }
-}
 
 };
 </script>
